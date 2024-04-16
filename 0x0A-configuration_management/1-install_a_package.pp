7ec85c5cd08327c346af11d8486a98b320aad625
Emmanuel Akalazu <138693982+Enetreal@users.noreply.github.com>
2024-04-16 10:17:23 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Using Puppet install flask
+# Using Puppet to install Flask and Werkzeug with specific versions
 
-package {'Flask':
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
